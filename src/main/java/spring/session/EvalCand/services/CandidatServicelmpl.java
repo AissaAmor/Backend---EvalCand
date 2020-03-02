c57d494bed9b86309bc481f9052691e587d8d99e
amor.aissa1993@gmail.com <aissaamor>
2020-03-02 12:20:32 +0100

    loadEmail

--- src/main/java/spring/session/EvalCand/services/CandidatServicelmpl.java
+++ src/main/java/spring/session/EvalCand/services/CandidatServicelmpl.java
@@ -5,17 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.session.EvalCand.entities.Candidat;
-import spring.session.EvalCand.entities.Coach;
 import spring.session.EvalCand.repositories.CandidatRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Service("Candidatservice")
 public class CandidatServicelmpl implements CandidatService{
 
-	
+
 	@Autowired
-	CandidatRepository candidatrepository ; 
-	
-	
+	CandidatRepository candidatrepository ;
+
+	@PersistenceContext
+	EntityManager em;
+
 	@Override
 	public void AjoutCandidat(Candidat candidat) {
 		candidatrepository.save(candidat);
@@ -26,6 +31,21 @@ public class CandidatServicelmpl implements CandidatService{
 	public void deleteCandidat(Candidat candidat) {
 		candidatrepository.delete(candidat);
 		
+	}
+
+	@Override
+	public Candidat getCandidatById(Integer id) {
+
+		return candidatrepository.findById(id).get();
+	}
+
+	@Override
+	public Candidat loadByEmailCand(String email) {
+
+			TypedQuery<Candidat> query = (TypedQuery<Candidat>) em
+					.createQuery("SELECT c FROM Candidat c WHERE c.Email = :email", Candidat.class);
+			Candidat candidat = query.setParameter("email", email).getSingleResult();
+			return candidat;
 	}
 
 	@Override
